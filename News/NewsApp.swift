20568d7327c3bab93ed78eb96a887160b1ec87ec
macAji <ajienergystar@gmail.com>
2025-05-19 06:02:59 +0700

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,20 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by mac on 19/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
